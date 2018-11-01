5ebad632583da4540e2e35cedc04bf9e06ead5f2
Philipp <41761223+philipp2310@users.noreply.github.com>
2018-11-01 16:43:08 +0100

    setup missing..

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#/usr/bin/env bash -e
+
+VENV=venv
+
+if [ ! -d "$VENV" ]
+then
+
+    PYTHON=`which python2`
+
+    if [ ! -f $PYTHON ]
+    then
+        echo "could not find python"
+    fi
+    virtualenv -p $PYTHON $VENV
+
+fi
+
+. $VENV/bin/activate
+
+pip install -r requirements.txt
+
+if [ ! -f ./.favs ]; then
+    touch .favs
+    sudo chown _snips-skills .favs
+fi
