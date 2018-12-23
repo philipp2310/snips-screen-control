0f57627a746293750c0c73e5a51f904280cb9adc
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2018-12-24 00:20:27 +0100

    Update de.py

--- translations/de.py
+++ translations/de.py
@@ -8,5 +8,10 @@ This module contains the result sentences and intents for the German version of 
 DEFAULT_TEXT_ON = "Bild an!"
 DEFAULT_TEXT_OFF = "Bild aus!"
 
+# Parameters
+PARA_TEXT_ON = 'text_an'
+PARA_TEXT_OFF = 'text_aus'
+PARA_FEEDBACK = 'akustisches_feedback'
+
 # Intents
 INTENT_TOGGLE_SCREEN = "Philipp:toggleScreen"
