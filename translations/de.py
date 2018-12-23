365c856ade411c607f78beb26d0c9ed36b3a6bc2
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2018-12-24 00:12:53 +0100

    Create de.py

---
+++ translations/de.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This module contains the result sentences and intents for the German version of the app
+"""
+
+# Default Texts
+DEFAULT_TEXT_ON = "Bild an!"
+DEFAULT_TEXT_OFF = "Bild aus!"
+
+# Intents
+INTENT_TOGGLE_SCREEN = "hermes/intent/Philipp:toggleScreen"
